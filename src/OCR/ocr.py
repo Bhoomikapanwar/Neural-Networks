da3287f57251a6156d337538b4e3068891dfc564
Bhoomikapanwar <bhoomika.mcs17.du@gmail.com>
2018-10-02 16:39:15 +0530

    Line removed

--- src/OCR/ocr.py
+++ src/OCR/ocr.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-#from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
